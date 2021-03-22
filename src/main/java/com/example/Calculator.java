00d561bc609c7d8919cc9770746c7a257dcd4938
salartelo <salarmail12@gmail.com>
2021-03-22 23:07:57 +0100

    Step 1: done

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.regex.Pattern;
+
+public class Calculator
+{
+    int add(String numbers){
+
+        if(numbers.length()<1)
+            return 0;
+
+        return Pattern.compile(",").splitAsStream(numbers).mapToInt(Integer::parseInt).sum();
+
+    }
+}
---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void CommaAdditionTest(){
+        Calculator calc = new Calculator();
+
+        var formula = "4,5,1";
+        assertEquals(10, calc.add(formula));
+        assertNotEquals(0, calc.add(formula));
+
+    }
+    @Test
+    void EmptyAdditionTest(){
+        Calculator calc = new Calculator();
+
+        var formula = "";
+        assertEquals(0, calc.add(formula));
+        assertNotEquals(Integer.MAX_VALUE, calc.add(formula));
+    }
+}
